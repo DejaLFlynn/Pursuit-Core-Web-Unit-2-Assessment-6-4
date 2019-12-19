dfb5e0d61254320763d151fca919f9509adac59e
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-18 21:49:39 -0500

    still working on the loop to append the year and title got the description to append

--- unit2.js
+++ unit2.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     let select = document.querySelector("select")
     let form = document.querySelector("form")
     let ul = document.querySelector("ul")
 
-    const getMovies =async()=>{
-        try{
+    const getMovies = async () => {
+        try {
             let res = await axios.get("https://ghibliapi.herokuapp.com/films")
             let films = res.data;
             // debugger
-            films.forEach(film=>{
+            films.forEach(film => {
                 let option = document.createElement("option")
                 let selected = document.querySelector("select")
                 option.innerText = film.title;
@@ -16,40 +16,44 @@ document.addEventListener("DOMContentLoaded",()=>{
                 option.value = film.description;
                 select.appendChild(option)
                 selected.appendChild(option)
-                
+
             })
 
-        }catch(err){
+        } catch (err) {
             console.log("error")
 
         }
     }
-        // const getTitle = async(url)=>{
-        //     try{
-        //         let res = await axios.get(url)
-        //         let title = res.data;
-        //          debugger
-        //         let section = document.querySelector("section")
-        //         if(section){
-        //             section.parentNode.removeChild(section)
-        //         }
-        //         section = document.createElement("section")
-        //         let h1 = document.createElement("h1")
-        //         h1.innerText = title.film
-        //         let p = document.createElement("p")
-        //         p.innerText = url
-        //         document.body.appendChild(section)
-        //         section.appendChild(h1)
-        //         section.appendChild(p)
+    // const getTitle = async (url) => {
+    //     try {
+    //         let res = await axios.get(url)
+    //         let titles = res.data;
+    //         titles.forEach(title => {
+    //             let section = document.querySelector("section")
+    //             if (section) {
+    //                 section.parentNode.removeChild(section)
+    //             }
+    //             section = document.createElement("section")
+    //             let h1 = document.createElement("h1")
+    //             h1.innerText = title.film
+    //             let p = document.createElement("p")
+    //             p.innerText = url
+    //             document.body.appendChild(section)
+    //             section.appendChild(h1)
+    //             section.appendChild(p)
 
-        //     }catch(err){
-        //         console.log("err")
-        //     }
-        // }
+    //         })
+
+
+    //     } catch (err) {
+    //         console.log("err")
+    //     }
+    // }
     const getDescription = async(url)=>{
         console.log(url)
+
         let section = document.querySelector("select")
-        
+
         if(section){
             section.parentNode.removeChild(section)
         }
@@ -58,23 +62,24 @@ document.addEventListener("DOMContentLoaded",()=>{
         let p = document.createElement("p")
         p.innerText = url
         document.body.appendChild(p)
-        let name = document.getElementById("select")
-        let title = name.options[name.selectedIndex].value
-        document.body.appendChild(title)
+        let title = document.createElement('h3')
+        title.innerText = url.title;
+        let year = document.createElement('p')
+        year.innerText = url.release_date
     }
-    select.addEventListener("change", (event)=>{
+    select.addEventListener("change", (event) => {
         getDescription(event.target.value)
-        // getTitle(event.target.value)
+        getTitle(event.target.value)
     })
     getMovies()
-    form.addEventListener("submit",(event)=>{
+    form.addEventListener("submit", (event) => {
         event.preventDefault()
         let li = document.createElement("li")
         let input = document.querySelector("input")
         li.innerText = `User Review: ${input.value}`
         ul.appendChild(li)
         input.value = ""
-        button.addEventListener("click", ()=>{
+        button.addEventListener("click", () => {
             ul.removeChild(li)
         })
     })
